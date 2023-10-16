4c7362006ad54c61daa311727f2010cd7974e776
hoshimotoyasunori <hoshimotoyasunori@gmail.com>
2023-10-16 14:11:20 +0900

    fase2-19

--- app/comment/page.jsx
+++ app/comment/page.jsx
@@ -3,7 +3,7 @@ import styles from '../form.module.css'
 const Comment =()=>{
     return (
         <session className={styles.container}>
-            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScyUHfWBmZ_oc5UFybwiSuIypSybBqTkZ0o2T6VY39Rph076A/viewform?embedded=true" width="100%" height="1000" marginheight="0" marginwidth="0">読み込んでいます…</iframe>
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScyUHfWBmZ_oc5UFybwiSuIypSybBqTkZ0o2T6VY39Rph076A/viewform?embedded=true" width="100%"  marginheight="0" marginwidth="0">読み込んでいます…</iframe>
         </session>
     )
 };
