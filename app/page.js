612a8d39da8e77668f3695b464a1f1ac8a42f919
hoshimotoyasunori <hoshimotoyasunori@gmail.com>
2023-10-16 10:38:52 +0900

    add color4

--- app/page.js
+++ app/page.js
@@ -20,18 +20,6 @@ export default function Home() {
             <button className={`${styles.button} ${styles.font}`}>ご意見・ご要望</button>
           </div>
         </div>
-      {/* footer */}
-        {/* <div>
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-        </div> */}
       </div>
       <div className={styles.grid}>
         <div className={styles.card1}>
@@ -52,6 +40,10 @@ export default function Home() {
           {/* <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfzwQar3vcxEA9u9eMweDUtzz5QG-rQe1MbK-mvK4YtWtcz2w/viewform?embedded=true" width="100%" height="auto" frameborder="0" marginheight="0" marginwidth="0">読み込んでいます…</iframe> */}
         </div>
       </div>
+      {/* footer */}
+        <div className={styles.footer}>
+            <p>©️2023 ES</p>
+        </div>
     </main>
   )
 }
