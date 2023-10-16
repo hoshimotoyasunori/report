71853b4589008d19aa6a4b03f7c557254f9e64ac
hoshimotoyasunori <hoshimotoyasunori@gmail.com>
2023-10-16 11:52:45 +0900

    fase2-2

--- app/report/page.jsx
+++ app/report/page.jsx
@@ -1,6 +1,10 @@
+import styles from '../page.module.css'
+
 const Report =()=>{
     return (
-        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdkDiKDT9qqvjEIAMHxtmkI2BQMIsArx7Ph71LQVfIX1yJAuA/viewform?embedded=true" width="100%" height="700" frameborder="0" marginheight="0" marginwidth="0">読み込んでいます…</iframe>
-    )
+            // <session className={styles.container}>
+                <iframe  src="https://docs.google.com/forms/d/e/1FAIpQLSdkDiKDT9qqvjEIAMHxtmkI2BQMIsArx7Ph71LQVfIX1yJAuA/viewform?embedded=true" width="100%" height="700" frameborder="0" marginheight="0" marginwidth="0">読み込んでいます…</iframe>
+            // </session>
+        )
 };
 export default Report;
