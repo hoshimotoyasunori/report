51e646be7d49152fc47ac2a51d80579e15ed49ca
hoshimotoyasunori <hoshimotoyasunori@gmail.com>
2023-10-17 13:26:34 +0900

    de11

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
       <div className={styles.grid}>
         <li>
-          <Link href='/report' className={`${styles.card} ${styles.card1}`}>
+          <Link href='/arrival' className={`${styles.card} ${styles.card1}`}>
             <span>STEP 1
               <h2>現着報告 <span>-&gt;</span></h2>
               <p>現着到着時刻を報告してください。</p>
@@ -13,7 +13,7 @@ export default function Home() {
           </Link>
         </li>
         <li>
-          <Link href='/arrival' className={`${styles.card} ${styles.card2}`}>
+          <Link href='/report' className={`${styles.card} ${styles.card2}`}>
             <span>STEP 2
               <h2>定時連絡 <span>-&gt;</span></h2>
               <p>活動の結果報告時にご使用ください。</p>
