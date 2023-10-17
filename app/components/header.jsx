3b79ed233ad2ce367e0f2dc4f61ca92b9c90af23
hoshimotoyasunori <hoshimotoyasunori@gmail.com>
2023-10-17 12:49:44 +0900

    de2

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useRouter } from 'next/navigation'
-// import Link from "next/link";
+import Link from "next/link";
 import styles from './header.module.css';
 
 const header =()=>{
@@ -9,10 +9,26 @@ const header =()=>{
     return (
         <header>
             <nav className={styles.nav}>
-                <button className={`${styles.button} ${styles.font}`} onClick={()=>router.push("/")}>Top</button>
-                <button className={`${styles.button} ${styles.font}`} onClick={()=>router.push("/arrival")}>現着報告</button>
-                <button className={`${styles.button} ${styles.font}`} onClick={()=>router.push("/report")}>定時連絡</button>
-                <button className={`${styles.button} ${styles.font}`} onClick={()=>router.push("/comment")}>ご意見・ご要望</button>
+                <li>
+                    <Link href='/' className={`${styles.button} ${styles.font}`}>
+                        <span>TOP</span>
+                    </Link>
+                </li>
+                <li>
+                    <Link href='/arrival' className={`${styles.button} ${styles.font}`}>
+                        <span>現着報告</span>
+                    </Link>
+                </li>
+                <li>
+                    <Link href='/report' className={`${styles.button} ${styles.font}`}>
+                        <span>定時連絡</span>
+                    </Link>
+                </li>
+                <li>
+                    <Link href='/comment' className={`${styles.button} ${styles.font}`}>
+                        <span>ご意見・ご要望</span>
+                    </Link>
+                </li>
             </nav>
         </header>
     );
